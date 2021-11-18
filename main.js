a0735894e01fcbabed8d6cfd2f9732a11fbbe429
Marco Tsang <marco@MacBook-Pro.local>
2021-11-18 08:08:22 +0000

    Hand in work for JS102

---
+++ main.js
@@ -0,0 +1,32 @@
+function authenticateUser() {
+  let attempts = 0;
+  while (attempts < 3){
+
+    let answer = prompt("Please enter the password.");
+    console.log(answer);
+
+
+    let userPassword = "myPassword1!";
+
+    if (answer === userPassword){
+      return true;
+      /*alert("My secret information here");*/
+      
+    } else {
+        attempts++;
+    }
+
+  }
+  return false;
+}
+
+function checkLoggedIn() {
+  let isLoggedIn = authenticateUser();
+
+
+  if ( isLoggedIn ){
+    alert("My secret information here");
+  }
+}
+
+checkLoggedIn();
